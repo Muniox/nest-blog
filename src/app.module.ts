68c05e9e5fd4c397c52e9009b5b77a8a503291b2
Muniox <muniox20@gmail.com>
2023-11-25 23:05:13 +0100

    [Feature] Created user CRUD resource
    - Created CRUD user resource
    - Created User entity with basic columns

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from "@nestjs/config";
 import { envValidationObjectSchema } from "./config";
+import { UserModule } from './user/user.module';
 
 
 @Module({
   imports: [ConfigModule.forRoot({
       validationSchema: envValidationObjectSchema,
-  }), DatabaseModule],
+  }), DatabaseModule, UserModule],
   controllers: [],
   providers: [],
 })
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,35 @@
+import {
+    BaseEntity,
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({
+    name: 'users',
+})
+export class User extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @Column({
+        unique: true,
+    })
+    email: string;
+
+    @Column()
+    hash: string;
+
+    @Column({
+        nullable: true,
+    })
+    hashedRT: string;
+}
