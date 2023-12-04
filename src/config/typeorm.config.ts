2c8a2893391493e77396d7e07d6334e2408a39ad
Muniox <muniox20@gmail.com>
2023-12-04 22:10:27 +0100

    [Refactor] Refactor typeorm config to improve readability
    The spacing of the properties within the TypeOrmModuleOptions object returned by getTypeOrmConfig has been adjusted. This alteration makes the code more standardized and easier to read, contributing to overall code quality. No changes to functionality were made.

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,17 +2,17 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const getTypeOrmConfig = (
-    configService: ConfigService,
+  configService: ConfigService,
 ): TypeOrmModuleOptions => ({
-    type: 'mysql',
-    host: configService.get<string>('DB_HOST'),
-    port: configService.get<number>('DB_PORT'),
-    username: configService.get<string>('DB_USERNAME'),
-    password: configService.get<string>('DB_PASSWORD'),
-    database: configService.get<string>('DB_DATABASE'),
-    bigNumberStrings: false,
-    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-    // autoLoadEntities: true,
-    synchronize: configService.get<boolean>('DB_SYNCHRONIZE'),
-    logging: configService.get<boolean>('DB_LOGGING'),
+  type: 'mysql',
+  host: configService.get<string>('DB_HOST'),
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USERNAME'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_DATABASE'),
+  bigNumberStrings: false,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  // autoLoadEntities: true,
+  synchronize: configService.get<boolean>('DB_SYNCHRONIZE'),
+  logging: configService.get<boolean>('DB_LOGGING'),
 });
