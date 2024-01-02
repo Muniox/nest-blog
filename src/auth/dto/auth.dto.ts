cbbf7137e5a41d04a5bea3d061343ba8fd17fca4
Muniox <muniox20@gmail.com>
2024-01-02 22:30:12 +0100

    [Feature] Password regexp
    'Password Must contain at least 1 lowercase letter, 1 uppercase letter, 1 digit, and 1 special character (!,@,#,$,%,^,&,*). Minimum 8 characters, maximum 24 characters.'

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -2,11 +2,11 @@ import {
   IsEmail,
   IsNotEmpty,
   IsString,
+  Matches,
   MaxLength,
   MinLength,
 } from 'class-validator';
 
-// TODO: dodać wyrazenie regularne aby sprawdzac password
 export class AuthDto {
   @IsEmail()
   @IsNotEmpty()
@@ -17,5 +17,12 @@ export class AuthDto {
   @IsNotEmpty()
   @MinLength(6)
   @MaxLength(16)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,12}$/,
+    {
+      message:
+        'Password Must contain at least 1 lowercase letter, 1 uppercase letter, 1 digit, and 1 special character (!,@,#,$,%,^,&,*). Minimum 8 characters, maximum 24 characters.',
+    },
+  )
   password: string;
 }
