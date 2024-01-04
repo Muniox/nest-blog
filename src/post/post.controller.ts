6bad3e2b84d27d4ac5d24678254019627c2f3f16
Muniox <muniox20@gmail.com>
2024-01-04 23:27:02 +0100

    [Feature] Added image file validation
    - validate max size
    - validate file/image type

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -10,12 +10,16 @@ import {
   UploadedFile,
   StreamableFile,
   Header,
+  ParseFilePipeBuilder,
+  HttpStatus,
 } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { Public, User } from '../auth/decorators';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { PostEntity } from './entities/post.entity';
+import { DeleteResult } from 'typeorm';
 
 @Controller('post')
 export class PostController {
@@ -26,25 +30,37 @@ export class PostController {
   async create(
     @Body() createPostDto: CreatePostDto,
     @User('sub') userId: string,
-    @UploadedFile() file: Express.Multer.File,
-  ) {
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'image/jpeg',
+        })
+        .addMaxSizeValidator({
+          maxSize: 1024,
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
+  ): Promise<{ message: string; statusCode: number }> {
     return await this.postService.create(createPostDto, userId, file);
   }
 
   @Get()
-  async findAll() {
+  async findAll(): Promise<PostEntity[]> {
     return await this.postService.findAll();
   }
 
   @Get('image/:filename')
   @Public()
   @Header('Content-Type', 'image/jpeg')
-  async getFile(@Param('filename') filename: string): Promise<StreamableFile> {
-    return await this.postService.getFile(filename);
+  getFile(@Param('filename') filename: string): StreamableFile {
+    return this.postService.getFile(filename);
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string): Promise<PostEntity> {
     return await this.postService.findOne(id);
   }
 
@@ -54,13 +70,25 @@ export class PostController {
     @User('sub') userId: string,
     @Param('id') id: string,
     @Body() updatePostDto: UpdatePostDto,
-    @UploadedFile() file: Express.Multer.File,
-  ) {
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'image/jpeg',
+        })
+        .addMaxSizeValidator({
+          maxSize: 1024,
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
+  ): Promise<{ message: string; statusCode: number }> {
     return await this.postService.update(id, updatePostDto, userId, file);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string): Promise<DeleteResult> {
     return await this.postService.remove(id);
   }
 }
