3bc3f625370ed3f31b606e0a6145638f4d561992
Muniox <muniox20@gmail.com>
2024-01-07 19:45:52 +0100

    [Fix] Fixed wrong password message validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -18,10 +18,10 @@ export class CreateUserDto {
   @MinLength(6)
   @MaxLength(16)
   @Matches(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,12}$/,
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,16}$/,
     {
       message:
-        'Password Must contain at least 1 lowercase letter, 1 uppercase letter, 1 digit, and 1 special character (!,@,#,$,%,^,&,*). Minimum 8 characters, maximum 24 characters.',
+        'Password Must contain at least 1 lowercase letter, 1 uppercase letter, 1 digit, and 1 special character (!,@,#,$,%,^,&,*). Minimum 6 characters, maximum 16 characters.',
     },
   )
   password: string;
