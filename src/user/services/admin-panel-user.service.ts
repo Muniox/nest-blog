4b997ea0e1fc285f927f39acbed8b29ca0163ab1
pawel.bartoszewski <pawel.bartoszewski@lge.com>
2024-11-21 14:25:54 +0100

    Added TODO for logout user function

--- src/user/services/admin-panel-user.service.ts
+++ src/user/services/admin-panel-user.service.ts
@@ -90,6 +90,7 @@ export class AdminPanelUserService {
     return await this.userService.removeUser(id);
   }
 
+  // @TODO: sprawdzić czy user istnije i zapisać zmiany, jeśli nie istnieje zwrócić błąd
   async logoutUser(id: string): Promise<void> {
     await this.userRepository.update(
       { id, hashedRT: Not(IsNull()) },
